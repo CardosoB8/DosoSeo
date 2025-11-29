6155ec89710ee90ab68b20f71c3aacd8b008ccc8
CardosoB8 <166424120+CardosoB8@users.noreply.github.com>
2025-11-29 19:38:50 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "upskittyan.com",
-    "zoneId": 9698203
+    "domain": "3nbf4.com",
+    "zoneId": 10254324
 }
 self.lary = ""
-importScripts('https://upskittyan.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
