349a45cc2b7e81c4b50624728474a0f008d2f315
CardosoB8 <166424120+CardosoB8@users.noreply.github.com>
2025-12-08 17:33:57 +0200

    Update links.js

--- data/links.js
+++ data/links.js
@@ -198,6 +198,12 @@ const links = [
     "original_url": "https://www.mediafire.com/file/ytd21gvvwvtse9m/internet-ilimtdo-mdo.ehi/file",
     "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
     "steps": 6
+  },
+  {
+    "alias": "bot-aviator-ui",
+    "original_url": "https://www.mediafire.com/file/21j9ioqc64lyvsr/BotAviator_v1.0_%2528com.bot.aviatorui%252C_1%2529_2025-12-08T172806.swb/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 6
   }
 ];
 
