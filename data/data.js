cfa995a098de73c9bcf4f418d14d21d8ffe5f014
CardosoB8 <166424120+CardosoB8@users.noreply.github.com>
2026-03-29 12:45:07 +0200

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,307 @@
+// data/links.js
+
+const links = [
+  {
+    "alias": "apna-tunnel",
+    "original_url": "https://play.google.com/store/apps/details?id=com.apnatunnel.lite",
+    "description": "Meu canal oficial no YouTube com tutoriais e dicas!",
+    "steps": 3
+  },
+  {
+    "alias": "arquivo-minapronet",
+    "original_url": "https://www.mediafire.com/file/7gjnbdu5v6jj33c/Net_Fast-MinaProNet-14Dias_20250903072607.mina/file",
+    "description": "Entre no nosso grupo do WhatsApp para interagir e receber novidades.",
+    "steps": 3
+  },
+  {
+    "alias": "fx-tunnel",
+    "original_url": "https://www.mediafire.com/file/b6x9zc0xrrrlarr/Fx_Tunnel_3.5.apks/file",
+    "description": "Fale comigo por e-mail para qualquer dúvida ou parceria.",
+    "steps": 3
+  },
+  {
+    "alias": "arquivo-ehi-mrdoso",
+    "original_url": "https://www.mediafire.com/file/208o5re460sek67/Net_Free_Mr_Doso.ehi/file",
+    "description": "Baixe a versão mais recente, cheio de novidades!",
+    "steps": 3
+  },
+  {
+    "alias": "starkvpn-arquivo-fast",
+    "original_url": "https://www.mediafire.com/file/jxs0i2mcsu63xn6/NetFast-SNI-MrDoso.stk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "ilimitado-linklayervpn",
+    "original_url": "https://www.mediafire.com/file/m2pnq4rln6nrrl5/ilimitado-linklayerVpn.lnk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "projeto-botaviatorv1",
+    "original_url": "https://www.mediafire.com/file/jr5flfoj8r56x4m/bot_v1.0_%2528com.bot.aviator%252C_1%2529_2025-09-15T092037.swb/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "ff-ilimitado",
+    "original_url": "https://www.mediafire.com/file/zqkfl98saw98m3f/ffIlimitado-mrdoso.tls/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "ilimitado-tcx-tunnel",
+    "original_url": "https://www.seodoso.xyz/txc-tunnel-config.txt",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "tcx-tunnel-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=org.tcx.tunnel.plus",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "projeto-botaviatorv1-novo",
+    "original_url": "https://www.mediafire.com/file/lfhjttgq64cbizr/bot+v1.0+(com.bot.aviator,+1)+2025-10-02T091417.swb/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "arquivo-tls-rapido",
+    "original_url": "https://www.mediafire.com/file/h63wuwcdfhpmrch/NetFastTLS.tls/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "aplicativo-tls-vpn",
+    "original_url": "https://play.google.com/store/apps/details?id=com.tlsvpn.tlstunnel",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "net-injector-vm",
+    "original_url": "https://www.mediafire.com/file/vdu443qj8fiwouu/NetFree02-mrdoso.ehi/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "net-http-shadow",
+    "original_url": "https://www.mediafire.com/file/giqllcoo6ogfetf/NetFreeIlimitado-mds.ehi/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "http-injector-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=com.evozi.injector",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "projeto-gerador-sinais",
+    "original_url": "https://www.mediafire.com/file/zjkf6vc9sdnbxcr/bot_v1.0_%2528com.bot.aviator%252C_1%2529_2025-10-15T080821.swb/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "sketchware-app",
+    "original_url": "https://www.mediafire.com/file/o6oazu8unzjnw1q/Sketchware_Pro_v7.0.0-SNAPSHOT-184aa07.apk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "tradutor-sk",
+    "original_url": "https://www.mediafire.com/file/pgy0nqi8tflgbde/Tradutor_SK_MrDoso_1.0.apk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "astro-vpn-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=xyz.tekidoer.austroplus.vpn",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "arquivo-atrovpn",
+    "original_url": "https://www.mediafire.com/file/2afdvapjks7med6/netfree-astrovpn-mrdoso.aro/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "projeto-botaviator",
+    "original_url": "https://www.mediafire.com/file/752jl2m8bbqz1za/bot_v1.0_%2528com.bot.aviator%252C_1%2529_2025-10-20T145730%25282%2529.swb/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "vpnilimitado-freenet",
+    "original_url": "https://www.mediafire.com/file/y11av5531ij0bz6/Xhypher_Tunnel_Pro_34.apks/filee",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "hkh-vpn",
+    "original_url": "https://play.google.com/store/apps/details?id=app.hackkcah.xyz",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "vpn-global-dt-mod",
+    "original_url": "https://www.mediafire.com/file/xfev55bjo1jn95d/vpnglobal-dt-4-4-7-mod.apk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "starkvpn-apk",
+    "original_url": "https://www.mediafire.com/file/um1ba19hxdz9596/Stark_VPN_Reloaded_7.1-rc.apk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "netfree-ilimitado",
+    "original_url": "https://www.mediafire.com/file/ysbz79q7okckq4w/NetFree-15d-starkvpn-mrdoso.stk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "netfast-starkvpn",
+    "original_url": "https://www.mediafire.com/file/2hz93rc9m5imxii/NetFast-15d-starkvpn-mrdoso.stk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "etapaone",
+    "original_url": "https://www.mediafire.com/file/glehx0x24su5mau/bot_v1.0_%2528com.bot.aviator%252C_1%2529_2025-11-17T084017.swb/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "ilimitado-arquivo-httpinjector",
+    "original_url": "https://www.mediafire.com/file/o6yqkrqo19z5jyr/HttpInjector4Dnv.ehi/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "fastnet-httpinjector",
+    "original_url": "https://www.mediafire.com/file/o6yqkrqo19z5jyr/HttpInjector4Dnv.ehi/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "internet-fast-mrdoso",
+    "original_url": "https://www.mediafire.com/file/a31bsxijcdvpjlo/internet-fast-mrdoso.ehi/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "internet-ilimitado-mrdoso",
+    "original_url": "https://www.mediafire.com/file/ytd21gvvwvtse9m/internet-ilimtdo-mdo.ehi/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "bot-aviator-ui",
+    "original_url": "https://www.mediafire.com/file/21j9ioqc64lyvsr/BotAviator_v1.0_%2528com.bot.aviatorui%252C_1%2529_2025-12-08T172806.swb/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "canais-tv-gratis",
+    "original_url": "https://www.mediafire.com/file/orvpzifov0suqkd/canais.txt/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "tv-mod-apk",
+    "original_url": "https://www.mediafire.com/file/8c6qa927is4ijmd/TV_Gr%25C3%25A1tis_Mod_1.0.apk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "tvcodes-free",
+    "original_url": "https://www.mediafire.com/file/l3j0v2xqpivpgtw/tvcodes.txt/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "ilimitado-custom",
+    "original_url": "https://www.mediafire.com/file/ebes25wvko4fbus/netfreeilimitado-mrdoso.hc/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "http-custom-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=xyz.easypro.httpcustom",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "ilimitado-dark",
+    "original_url": "https://www.mediafire.com/file/9u1aumjgpkdj1pb/Dark4diasNv.dark/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "dark-tunnel-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=net.darktunnel.app",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "dark-ilimitado",
+    "original_url": "https://www.mediafire.com/file/9u1aumjgpkdj1pb/Dark4diasNv.dark/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "astroilimitado-fast",
+    "original_url": "https://www.mediafire.com/file/k2dqqd1jrz73sww/AustroVpn4dNv.aro/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "iptunnel-ilimitadofast",
+    "original_url": "https://www.mediafire.com/file/h5x4hhaluwywdba/ilimitado-iptunnel.ipt/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "astrovpn-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=xyz.tekidoer.austroplus.vpn",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "iptunnelvpn-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=com.iptunnel.tunnel",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+    {
+    "alias": "layervpn-netfree",
+    "original_url": "https://www.mediafire.com/file/oak4n4ciz9q0yyo/linklayernetfree.lnk/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+    },
+    {
+    "alias": "layer-vpn-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=com.newtoolsworks.linklayer",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+    },
+  {
+    "alias": "minapronet-ilimitado",
+    "original_url": "https://www.mediafire.com/file/al1xmgnzmwgraqs/minanet4dNv_20260328084041.mina/file",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  },
+  {
+    "alias": "minapronet-apk",
+    "original_url": "https://play.google.com/store/apps/details?id=vpn.minapronet.com.eg",
+    "description": "Um documento essencial sobre as melhores práticas de desenvolvimento.",
+    "steps": 3
+  }
+];
+
+// Exporta o array de links para que index.js possa importá-lo
+module.exports = links;
